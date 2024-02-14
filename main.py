fcfcb54edc9b4bd4a47a3aa2f99fbe06b387ec74
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2024-02-14 12:29:51 +0100

    reworded

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from datetime import datetime
 from peoplecounter import count_people
 
 st.set_page_config(layout="wide")
-st.title("Meeting Cost calculator")
-st.subheader("€ per hour x people in the call x time")
 
 # Initialize a DataFrame to store data
 data = pd.DataFrame(columns=['Timestamp', 'People Count', 'Total Time (minutes)', 'Total Cost (€)'])
@@ -18,6 +16,7 @@ def reset_data():
 def main():
     global data  # Declare data as a global variable
 
+    test = 1
     # Add widgets to the sidebar
     st.sidebar.header("Settings")
     hourly_wage = st.sidebar.number_input(label='Hourly wage', value=50)
@@ -26,24 +25,29 @@ def main():
     if reset_button:
         reset_data()
 
+
     with st.empty():
-        
         while True:
             # Get current timestamp
             timestamp = datetime.now()
 
-
             # Call the function to count people
             people_count = count_people()
+            last_three_zeros = data['People Count'].tail(3).eq(0).all()
+
+            if last_three_zeros:
+                reset_data()
 
             # Calculate the total time in meeting per minute
             if not data.empty:
                 total_time = (timestamp - data.iloc[0]['Timestamp']).total_seconds() / 60
+                total_cost = total_time * (people_count * hourly_wage / 60)
             else:
                 total_time = 0
+                total_cost = 0
 
-            # Calculate the total cost of the meeting
-            total_cost = total_time * (people_count * hourly_wage / 60)
+            # Update the title with the dynamic value
+            st.title(f"This meeting cost: {round(total_cost, 2)} €")
 
             # Append data to the DataFrame
             data = data.append({
@@ -57,10 +61,9 @@ def main():
 
             # Calculate delta based on the previous row
             delta_people_count = data['People Count'].diff().iloc[-1] if len(data) > 1 else 0
-
-            col1.metric("People Count", value=round(people_count), delta=delta_people_count, delta_color='inverse')
-            col2.metric("Total Time (minutes)", value=round(total_time, 2))
-            col3.metric("Total Cost (€)", value=round(total_cost, 2), delta_color='inverse')
+            col1.metric("This meeting cost (€)", value=round(total_cost, 2), delta_color='inverse')
+            col3.metric("People currently not working efficiently", value=round(people_count), delta=delta_people_count, delta_color='inverse')
+            col2.metric("This meeting took (minutes)", value=round(total_time, 2))
             #col4.dataframe(data)
 
             # Sleep for a minute (adjust as needed)
